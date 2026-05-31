fcede16a037a67ef33f007861590c0ff37665133
Tony-123-a11y <ayushsinghatschool@gmail.com>
2026-05-31 23:43:03 +0530

    chore:API_KEY updage

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -3,6 +3,6 @@ import { Client, TablesDB, ID } from "node-appwrite";
 const client = new Client()
   .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT!)
   .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID!)
-  .setKey(process.env.APPWRITE_FUNCTION_API_KEY!);
+  .setKey(process.env.APPWRITE_API_KEY!);
 
 export const tablesDB = new TablesDB(client);
